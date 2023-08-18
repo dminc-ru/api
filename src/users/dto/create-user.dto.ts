748e969bd39fb9750243259e18f86a0fa8eabf3d
Dan <dan@dminc.ru>
2023-08-18 21:01:04 +0300

    feat: add username field for the user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,19 @@
-import {ApiProperty} from "@nestjs/swagger";
-import {IsEmail, IsString, Length} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
-    @ApiProperty({example: 'user@example.com', description: 'Почта'})
-    @IsString({message: 'Должно быть строкой'})
-    @IsEmail({}, {message: 'Некорректный email'})
-    readonly email: string;
-    @ApiProperty({example: '123456789', description: 'Пароль'})
-    @IsString({message: 'Должно быть строкой'})
-    @Length(4, 16, {message: "Не меньше 4 и не больше 16"})
-    readonly password: string;
+  @ApiProperty({ example: "kentithun1", description: "Имя" })
+  @IsString({ message: "Должно быть строкой" })
+  @Length(3, 32, { message: "Не меньше 3 и не больше 32 символов" })
+  readonly username: string;
+
+  @ApiProperty({ example: "user@example.com", description: "Почта" })
+  @IsString({ message: "Должно быть строкой" })
+  @IsEmail({}, { message: "Некорректный email" })
+  readonly email: string;
+
+  @ApiProperty({ example: "123456789", description: "Пароль" })
+  @IsString({ message: "Должно быть строкой" })
+  @Length(4, 16, { message: "Не меньше 4 и не больше 16" })
+  readonly password: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,35 +1,68 @@
-import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
-import {ApiProperty} from "@nestjs/swagger";
-import {Role} from "../roles/roles.model";
-import {UserRoles} from "../roles/user-roles.model";
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { ApiProperty } from "@nestjs/swagger";
+import { Role } from "../roles/roles.model";
+import { UserRoles } from "../roles/user-roles.model";
 
 interface UserCreationAttrs {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-@Table({tableName: 'users'})
+@Table({ tableName: "users" })
 export class User extends Model<User, UserCreationAttrs> {
-    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
-    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
-    id: number;
+  @ApiProperty({
+    example: "1",
+    description: "Уникальный идентификатор пользователя",
+  })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
 
-    @ApiProperty({example: 'user@example.com', description: 'Почтовый адрес'})
-    @Column({type: DataType.STRING, unique: true, allowNull: false})
-    email: string;
+  @ApiProperty({
+    example: "kentithun1",
+    description: "Уникальное имя пользователя",
+  })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+  })
+  username: string;
 
-    @ApiProperty({example: '123456789', description: 'Пароль'})
-    @Column({type: DataType.STRING, allowNull: false})
-    password: string;
+  @ApiProperty({
+    example: "kentithun1@dminc.ru",
+    description: "Адрес электронного ящика пользователя",
+  })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
 
-    @ApiProperty({example: 'true', description: 'Забанен или нет'})
-    @Column({type: DataType.BOOLEAN, defaultValue: false})
-    banned: boolean;
+  @ApiProperty({ example: "qwerty12345", description: "Пароль пользователя" })
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
 
-    @ApiProperty({example: '1', description: 'Причина блокировки'})
-    @Column({type: DataType.STRING, allowNull: true})
-    banReason: string;
+  @ApiProperty({
+    example: "true",
+    description: "Наличие блокировки у пользователя",
+  })
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  banned: boolean;
 
-    @BelongsToMany(() => Role, () => UserRoles)
-    roles: Role[];
+  @ApiProperty({
+    example: "Нарушение Пользовательского соглашения dminc",
+    description: "Причина блокировки пользователя (при её наличии)",
+  })
+  @Column({ type: DataType.STRING, allowNull: true })
+  banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
