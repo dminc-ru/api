07ab4387f81fd236dab4118fbf76450b7e342fda
Dan <dan@dminc.ru>
2023-08-18 21:02:17 +0300

    style: apply prettifier

--- src/roles/user-roles.model.ts
+++ src/roles/user-roles.model.ts
@@ -1,22 +1,35 @@
-import {BelongsToMany, Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
-import {ApiProperty} from "@nestjs/swagger";
-import {User} from "../users/users.model";
-import {Role} from "./roles.model";
+import {
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "../users/users.model";
+import { Role } from "./roles.model";
 
-@Table({tableName: 'user_roles', createdAt: false, updatedAt: false})
+@Table({ tableName: "user_roles", createdAt: false, updatedAt: false })
 export class UserRoles extends Model<UserRoles> {
-    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
-    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
-    id: number;
+  @ApiProperty({ example: "1", description: "Уникальный идентификатор" })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
 
-    @ForeignKey(() => Role)
-    @ApiProperty({example: '123', description: 'Уникальный идентификатор роли'})
-    @Column({type: DataType.INTEGER})
-    roleId: number;
+  @ForeignKey(() => Role)
+  @ApiProperty({ example: "123", description: "Уникальный идентификатор роли" })
+  @Column({ type: DataType.INTEGER })
+  roleId: number;
 
-    @ForeignKey(() => User)
-    @ApiProperty({example: '456', description: 'Уникальный идентификатор пользователя'})
-    @Column({type: DataType.INTEGER})
-    userId: number;
-
+  @ForeignKey(() => User)
+  @ApiProperty({
+    example: "456",
+    description: "Уникальный идентификатор пользователя",
+  })
+  @Column({ type: DataType.INTEGER })
+  userId: number;
 }
