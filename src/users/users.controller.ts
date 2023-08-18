f5ea80fe2d76b1a51cb9414cb3bc933c8b969cc1
Dan <dan@dminc.ru>
2023-08-18 19:36:22 +0300

    feat: create UserService

---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {Body, Controller, Get, Post, UseGuards, UsePipes} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {User} from "./users.model";
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {RolesGuard} from "../auth/roles.guard";
+import {Roles} from "../auth/roles-auth.decorator";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
+import {ValidationPipe} from "../pipes/validation.pipe";
+
+@ApiTags('Пользователи')
+@Controller('/users')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @ApiOperation({summary: 'Создание пользователя'})
+    @ApiResponse({status: 200, type: User})
+    @UsePipes(ValidationPipe)
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+
+    @ApiOperation({summary: 'Получение всех пользователей'})
+    @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get()
+    getAll() {
+        return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Выдача роли'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
+    }
+}
